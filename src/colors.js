75d40f7bd6b44ff6ea3bb3409989e2f2d317f2a0
Shuffledex <shuffledex@gmail.com>
2021-04-28 09:16:25 -0300

    new colors

--- src/colors.js
+++ src/colors.js
@@ -1,12 +1,13 @@
 export default [
-  '#01888c', // teal
-  '#fc7500', // bright orange
-  '#034f5d', // dark teal
-  '#f73f01', // orangered
-  '#fc1960', // magenta
-  '#c7144c', // raspberry
-  '#f3c100', // goldenrod
-  '#1598f2', // lightning blue
-  '#2465e1', // sail blue
-  '#f19e02', // gold
+	'#00D8A0',
+  '#00C4B4',
+  '#00ADC2',
+  '#0095C5',
+  '#007CBC',
+  '#0061A7',
+  '#CF19E5',
+  '#FF4E76',
+  '#FF9250',
+  '#FFCA4A',
+  '#F9F871',
 ];
